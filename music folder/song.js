c216c4c2a222bab164c6b92ec15fc910e8af0224
gaugayathri <113286359+gaugayathri@users.noreply.github.com>
2023-01-03 11:44:22 +0530

    Add files via upload

---
+++ music folder/song.js
@@ -0,0 +1,64 @@
+let songs =[
+    {
+        name:'Love Me Like You Do',
+        path:'audios/Love Me Like You Do(PagalWorldl).mp3',
+        artist:'Ellie Goulding',
+        cover:'pictures/ellie goulding.jpg'
+    },
+    {
+        name:'Who Says',
+        path:'audios/Who Says(PagalWorld).mp3',
+        artist:'Selena Gomez',
+        cover:'pictures/Selena-Gomez.jpg'
+    },
+    {
+        name:'Dreamers',
+        path:'audios/Dreamers Jungkook New Song Download Mp3(SongsZilla.Net).mp3',
+        artist:'Jungkook',
+        cover:'pictures/jungkook.jpg'
+    },
+    {
+        name:'Baby',
+        path:'audios/Baby(PagalWorld).mp3',
+        artist:'Justin Biber',
+        cover:'pictures/justin biber.jpg'
+    },
+    {
+        name:'Ghar-More-Pardesiya',
+        path:'audios/Ghar-More-Pardesiya-MassTamilan.io.mp3',
+        artist:'Sherya Ghosal',
+        cover:'pictures/sherya ghoshal.jpg'
+    },
+    {
+        name:'Permission-to-Dance',
+        path:'audios/Permission-to-Dance.mp3',
+        artist:'Bts',
+        cover:'pictures/bts.jpg'
+    },
+    {
+        name:'Roar',
+        path:'audios/Roar - Katy Perry 320-(PagalWorld).mp3',
+        artist:'Kety Pery',
+        cover:'pictures/kety pery.jpg'
+    },
+    {
+        name:'Un-Vizhigalil',
+        path:'audios/Un-Vizhigalil.mp3',
+        artist:'Shruthi Hasan',
+        cover:'pictures/ShrutiHaasan.jpg'
+    },
+    {
+        name:'Vaaya-En-Veera',
+        path:'audios/Vaaya-En-Veera.mp3',
+        artist:'Sri',
+        cover:'pictures/sri.jpg'
+    },
+    {
+        name:'Yaarumilla',
+        path:'audios/Yaarumilla.mp3',
+        artist:'Swetha Mohan',
+        cover:'pictures/Shweta Mohan.jpg'
+    },
+        
+
+]
